c79fb4d26ab4bdbefbb5c841f1a50641a569a81d
Ottis Jennifer <105177827+Ojcee@users.noreply.github.com>
2023-02-14 15:15:00 +0100

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,52 +1,22 @@
 #!/usr/bin/python3
-"""Unittest module for the Amenity Class."""
-
-import unittest
-from datetime import datetime
-import time
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
-import re
-import json
-from models.engine.file_storage import FileStorage
 import os
-from models import storage
-from models.base_model import BaseModel
 
 
-class TestAmenity(unittest.TestCase):
+class test_Amenity(test_basemodel):
+    """ amenity test class"""
 
-    """Test Cases for the Amenity class."""
+    def __init__(self, *args, **kwargs):
+        """inti the test class """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
 
-    def setUp(self):
-        """Sets up test methods."""
-        pass
-
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
-
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
-
-    def test_8_instantiation(self):
-        """Tests instantiation of Amenity class."""
-
-        b = Amenity()
-        self.assertEqual(str(type(b)), "<class 'models.amenity.Amenity'>")
-        self.assertIsInstance(b, Amenity)
-        self.assertTrue(issubclass(type(b), BaseModel))
-
-    def test_8_attributes(self):
-        """Tests the attributes of Amenity class."""
-        attributes = storage.attributes()["Amenity"]
-        o = Amenity()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_name2(self):
+        """testing name type """
+        new = self.value()
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
